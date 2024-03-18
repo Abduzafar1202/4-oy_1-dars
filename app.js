3400f897f08d1d88f752e37878d184a3b2a7c4ea
Abduzafar1202 <aa908870138@gmail.com>
2024-03-18 19:40:23 +0500

    zafar

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-let elNav = document.querySelector(".header_container-nav");
-let elBtn1 = document.querySelector(".btn1");
-let elBtn2 = document.querySelector(".btn2");
-let elBtn = document.querySelector(".zafar");
+// let elNav = document.querySelector(".header_container-nav");
+// let elBtn1 = document.querySelector(".btn1");
+// let elBtn2 = document.querySelector(".btn2");
+// let elBtn = document.querySelector(".zafar");
 
-elBtn.addEventListener("click" , () => {
+// elBtn.addEventListener("click" , () => {
     
-    elNav.classList.toggle("d-none")
+//     elNav.classList.toggle("d-none")
     
-})
+// })
 
-elBtn.addEventListener("click" , () => {
+// elBtn.addEventListener("click" , () => {
     
-    elBtn1.classList.toggle("d-none")
+//     elBtn1.classList.toggle("d-none")
     
-})
+// })
 
-elBtn.addEventListener("click" , () => {
+// elBtn.addEventListener("click" , () => {
     
-    elBtn2.classList.toggle("d-none")
+//     elBtn2.classList.toggle("d-none")
     
-})
+// })
